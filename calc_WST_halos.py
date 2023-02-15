3589ae7f36fc0fe6dfea6804138dcd2cede2f349
RiccGerva97 <led.bluegrass@gmail.com>
2023-02-15 14:35:57 +0100

    minor changes in calc_WST_halos.py

--- calc_WST_halos.py
+++ calc_WST_halos.py
@@ -3,6 +3,7 @@ sys.path.insert(1, './MyFunc')
 from CalcWST import HaloWST_f
 
 import pickle
+import os
 
 # define desired redshift
 snapnum = 2
@@ -10,8 +11,8 @@ z_dict = {4:0.0, 3:0.5, 2:1.0, 1:2.0, 0:3.0}
 redshift = z_dict[snapnum]
 
 # define output name files with coefficients 
-first_file = 'first_order'
-second_file = 'second_order'
+Ff = 'first_order'
+Sf = 'second_order'
 
 # define root path where to find hale catalogues 
 root = '/media/fuffolo97/HDD1/UNI/Tesi/Halos/'
@@ -24,31 +25,39 @@ second_order_coeffs = []
 
 # loop over cosmologies
 for folder in folders:
+    
+    if os.path.exists(folder+Ff):
+        os.remove(folder+Ff)
+    if os.path.exists(folder+Sf):
+        os.remove(folder+Sf)
+    
     # loop over the different realizations
     for i in range(350):
         snapdir = root + folder +'/%d'%i
-        HaloWST_f(first_file, second_file, snapdir)
+        HaloWST_f(folder+Ff, folder+Sf, snapdir)
 
-# for i in range(350):
-#     with open(first_file, 'rb') as Ff:
-#         while True:
-#             try:
-#                 first_order_coeffs.append(pickle.load(Ff))
-#                 # do things
-#             except EOFError:
-#                 break
-#             # possible
-#             # else:
-#             # ...
+for folder in folders:
+    for i in range(350):
+        with open(folder+Ff, 'rb') as Ff:
+            while True:
+                try:
+                    first_order_coeffs.append(pickle.load(Ff))
+                    # do things
+                except EOFError:
+                    break
+                # possible
+                # else:
+                # ...
 
-# for i in range(350):
-#     with open(second_file, 'rb') as Sf:
-#         while True:
-#             try:
-#                 second_order_coeffs.append(pickle.load(Sf))
-#                 # do things
-#             except EOFError:
-#                 break
-#             # possible
-#             # else:
-#             # ...
+for folder in folders:
+    for i in range(350):
+        with open(folder+Sf, 'rb') as Sf:
+            while True:
+                try:
+                    second_order_coeffs.append(pickle.load(Sf))
+                    # do things
+                except EOFError:
+                    break
+                # possible
+                # else:
+                # ...
